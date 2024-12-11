2a200d84d3cd4cb72f4fd077a19d27898c1c8076
Marko Šutić <marko.sutic@mithril.eu>
2024-12-11 04:04:43 +0100

    Add initial project setup with Book management structure

---
+++ edu-8/src/main/java/eu/mithril/java/edu8/Book.java
@@ -0,0 +1,33 @@
+package eu.mithril.java.edu8;
+
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return year + " - " + title + " by " + author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+}
---
+++ edu-8/src/main/java/eu/mithril/java/edu8/BookManager.java
@@ -0,0 +1,22 @@
+package eu.mithril.java.edu8;
+
+import java.util.ArrayList;
+
+public class BookManager {
+
+    private ArrayList<Book> books;
+
+    public BookManager() {
+        books = new ArrayList<>();
+    }
+
+    // TODO: Add method to add a new book
+
+    // TODO: Add method to remove book by title
+
+    // TODO: Add method to find books by author
+
+    // TODO: Add method to find books published after certain year
+
+    // TODO: Add method to print all books
+}
